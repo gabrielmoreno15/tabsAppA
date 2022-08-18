1f9a5144dc5e779f1b750b150835501127f5f668
Carlos Gabriel Moreno <cm224590@gmail.com>
2022-08-18 12:15:48 -0300

    Initial commit

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { ApiService } from '../services/api.service';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  constructor(private api: ApiService) {
+    this.api.buscarPrevisao().subscribe(dados =>{
+      console.log(dados);
+    });
+  }
+
+
+
+}
